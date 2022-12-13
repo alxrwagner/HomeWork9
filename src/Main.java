b64323d56b6f929af352b14207fd58ac00a44c46
Alex Wagner <alxr.wagner@gmail.com>
2022-12-13 12:59:36 +0700

    Добавил методы toString(), hashCode(), equals(). Задание выполнил.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        Author terryPratchett = new Author("Терри", "Пратчетт");
+        Author joanneRowling = new Author("Джоан", "Роулинг");
+
+        Book harryPotterPart1 = new Book("Гарри Поттер и филосовский камень", 1997, joanneRowling);
+        Book theWitchersAbroad = new Book("Ведьмы за границей", 1991, terryPratchett);
+
+        System.out.println(theWitchersAbroad.toString() + "\n" + harryPotterPart1.toString());
+        System.out.println(theWitchersAbroad.hashCode());
 
     }
 }
